c5c40a436daed22d62a764621d9219f36fd7715c
succcubbus <jannis.kaiser2@gmail.com>
2018-06-24 13:34:14 +0200

    add usage instructions to readme, remove example call from code

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 A kotlin library providing a while-else functionality.
 
 This is meant as a joke, for which the idea arose from silly talk.
+
+Usage:
+
+```kotlin
+While(::canHasCookie) {
+    println("i got cookie")
+} Else {
+    println("i got no cookie :(")
+}
+```
--- src/main/kotlin/com/succcubbus/whileelse/WhileElse.kt
+++ src/main/kotlin/com/succcubbus/whileelse/WhileElse.kt
@@ -1,17 +1,5 @@
 package com.succcubbus.whileelse
 
-fun main(args: Array<String>) {
-    While(::canHasCookie) {
-        println("i got cookie")
-    } Else {
-        println("i got no cookie :(")
-    }
-}
-
-fun canHasCookie(): Boolean {
-    return Math.random() < 0.6
-}
-
 public fun <T> While(condition: () -> Boolean, block: () -> T): WhileElseResult<T?> {
     var result: T? = null
     var loops = 0L
